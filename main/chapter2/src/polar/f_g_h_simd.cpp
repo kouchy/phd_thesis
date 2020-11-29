6d1b6cd177670dd1550b20d9d4e8a6f1439c2efe
Adrien Cassagne <adrien.cassagne@inria.fr>
2020-11-29 21:06:38 +0100

    Cosmetics.

--- main/chapter2/src/polar/f_g_h_simd.cpp
+++ main/chapter2/src/polar/f_g_h_simd.cpp
@@ -5,16 +5,16 @@ class API_polar_SIMD : public API_polar
 	static void f(const R *la, const R *lb, R *lc, const int n_elmts) {
 		for (auto n = 0; n < n_elmts; n += mipp::N<R>())
 		{   // lc = f(la,lb) = sign(la.lb).min(|la|, |lb|)
-			auto r = mipp::neg(mipp::min(mipp::abs(&la[n]), mipp::abs(&lb[n])),
-			                   mipp::sign(&la[n], &lb[n]));
+			auto r = mipp::copysign(mipp::min(mipp::abs(&la[n]), mipp::abs(&lb[n])),
+			                        mipp::sign(&la[n], &lb[n]));
 			r.store(&lc[n]);
 		}
 	}
 	template <int N_ELMTS = 0> // <- this template parameter is not used here
-	static void g(const R *la, const R *lb, const B *sa, R *lc, const int n_elmts) {
+	static void g(const R *la, const R *lb, const B *s, R *lc, const int n_elmts) {
 		for (auto n = 0; n < n_elmts; n += mipp::N<R>())
-		{   // lc = g(la,lb,sa) = (1-2sa)la + lb
-			auto r = mipp::neg(&la[n], &sa[n]) + &lb[n];
+		{   // lc = g(la,lb,s) = (1-2s)la + lb
+			auto r = mipp::copysign(&la[n], &s[n]) + &lb[n];
 			r.store(&lc[n]);
 		}
 	}
